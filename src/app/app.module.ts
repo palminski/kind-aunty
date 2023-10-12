710252f8b829bd1b7293d95e0b3e566fa1662e7f
palminski <williambolls98@gmail.com>
2023-10-12 13:10:23 -0700

    added functionality for forms so that they may be handled my email package in future

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,7 +30,8 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/contact/message.ts
@@ -0,0 +1,6 @@
+export class Message {
+    constructor(
+        public test1: string,
+        public test2: string
+    ) {}
+}
